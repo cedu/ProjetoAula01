a7f0a72f049580425c9c92ed196db881cf346611
cedu <ceduferreira2@gmail.com>
2024-04-03 21:59:28 -0300

    first commit

---
+++ ProjetoAula01/Program.cs
@@ -0,0 +1,37 @@
+﻿using ProjetoAula01.Entities;
+using ProjetoAula01.Repositories;
+
+Console.WriteLine("\n*** CADASTRO DE CLIENTES: ***\n");
+
+//criando um objeto 
+var cliente = new Cliente();
+
+//gerando o id e a data e hora de cadastro
+cliente.Id = Guid.NewGuid();
+cliente.DataHoraCadastro = DateTime.Now;
+
+//usuário preenche o nome
+Console.Write("INFORME O NOME DO CLIENTE..: ");
+cliente.Nome = Console.ReadLine();
+
+//usuário preenche o email
+Console.Write("INFORME O EMAIL DO CLIENTE.: ");
+cliente.Email = Console.ReadLine();
+
+//usuário preenche o cpf
+Console.Write("INFORME O CPF DO CLIENTE...: ");
+cliente.Cpf = Console.ReadLine();
+
+//imprimindo os dados do cliente no console
+Console.WriteLine("\nDADOS DO CLIENTE:");
+Console.WriteLine("\tID...........: " + cliente.Id);
+Console.WriteLine("\tNOME.........: " + cliente.Nome);
+Console.WriteLine("\tEMAIL........: " + cliente.Email);
+Console.WriteLine("\tCPF..........: " + cliente.Cpf);
+Console.WriteLine("\tCADASTRADO EM: " + cliente.DataHoraCadastro);
+
+//instanciando a classe ClienteRepositorio
+var clienteRepository = new ClienteRepository();
+clienteRepository.ExportarDados(cliente);
+
+Console.WriteLine("\nDADOS GRAVADOS COM SUCESSO!\n");
---
+++ ProjetoAula01/Repositories/ClienteRepository.cs
@@ -0,0 +1,47 @@
+﻿using ProjetoAula01.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula01.Repositories
+{
+    public class ClienteRepository
+    {
+        /*  Método para exportar os dados do cliente para um arquivo de extensão txt */
+        public void ExportarDados(Cliente cliente)
+        {
+            try
+            {
+                //variavel para armazenar o caminho do arquivo
+                var path = "C:\\Users\\Carlos Eduardo\\source\\repos\\Turma-SQS-2024\\Arquivo\\";
+
+                //verificar se a pasta não existe
+                if (!Directory.Exists(path))
+                {
+                    //criando a pasta
+                    Directory.CreateDirectory(path);
+                }
+
+                //abrindo um arquivo em modo de escrita
+                var streamWriter = new StreamWriter(path + "\\clientes.txt", true);
+
+                //escrevendo conteudo do arquivo
+                streamWriter.WriteLine("ID DO CLIENTE......" + cliente.Id);
+                streamWriter.WriteLine("NOME..............." + cliente.Nome);
+                streamWriter.WriteLine("EMAIL.............." + cliente.Email);
+                streamWriter.WriteLine("CPF................" + cliente.Cpf);
+                streamWriter.WriteLine("CADASTRADO EM......" + cliente.DataHoraCadastro);
+                streamWriter.WriteLine("...");
+
+                //fechando o arquivo
+                streamWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu um erro ao escrever no arquivo: " + ex.Message);
+            }
+        }
+    }
+}
